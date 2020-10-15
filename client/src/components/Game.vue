cb37d757b02aa9847066bb63c42b0f695db2b758
GITrans <109586368+GITrans@users.noreply.github.com>
2020-10-15 16:52:54 +0000

    fix bags

--- client/src/components/Game.vue
+++ client/src/components/Game.vue
@@ -273,4 +273,6 @@ export default {
             console.log('this.move', this.move);
         },
         getSecret() {
-            return Math.random().toString(36)
+            return Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
+        },
+        getMoveHas
