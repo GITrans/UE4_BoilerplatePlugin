f37760e6f4889d2842949f6e79789b51a8afc638
GITrans <109586368+GITrans@users.noreply.github.com>
2020-12-19 19:08:32 +0000

    update ad

--- client/src/components/GameOpen.vue
+++ client/src/components/GameOpen.vue
@@ -35,3 +35,90 @@
                     <img src="/vader_t2.png" alt="avatar">
                 </v-avatar>
                 <v-avatar class="v-btn v-btn--depressed theme--light v-btn--floating"
+                    :size="120"
+                    :color="player === PlayerToIndex.yoda ? '' : '#1E1E1E'"
+                    v-on:click.stop="choosePlayer(PlayerToIndex.yoda)"
+                >
+                    <img src="/yoda_t2.png" alt="avatar">
+                </v-avatar>
+                <br>
+            </v-flex>
+            <v-flex xs12>
+                <v-btn
+                    v-on:click.stop="chooseMove(MovesToIndex.rock)"
+                    :light="move === MovesToIndex.rock ? true : false"
+                    large fab depressed
+                >
+                    <v-icon dark x-large>fa-hand-rock</v-icon>
+                </v-btn>
+                <v-btn
+                    v-on:click.stop="chooseMove(MovesToIndex.paper)"
+                    :light="move === MovesToIndex.paper ? true : false"
+                    large fab depressed
+                >
+                    <v-icon dark x-large>fa-hand-paper</v-icon>
+                </v-btn>
+                <v-btn
+                    v-on:click.stop="chooseMove(MovesToIndex.scissors)"
+                    :light="move === MovesToIndex.scissors ? true : false"
+                    large fab depressed
+                >
+                    <v-icon dark x-large class="fa-rotate-90">fa-hand-scissors</v-icon>
+                </v-btn>
+            </v-flex>
+            <v-flex xs12>
+                <br>
+                <div class="subheading">You will pay {{parseFloat(gameGuardianAmount / 10**18).toFixed(18)}} WETH.</div>
+            </v-flex>
+            <v-flex xs12>
+                <br><br>
+                <v-btn
+                    v-on:click.stop="play()"
+                    large fab
+                    :disabled="!player || !move || sendingPlay"
+                >
+                    <v-icon light x-large>fa-check</v-icon>
+                </v-btn>
+                <p class="subheading" text-xs-center wrap v-if="sendingPlay">
+                    Sending Raiden payment from your node..
+                </p>
+            </v-flex>
+        </v-layout>
+    </v-container>
+</template>
+
+<script>
+import { MovesToIndex, PlayerToIndex, GameGuardian } from '../constants';
+import Timer from './Timer';
+
+export default {
+    props: ['timer', 'player', 'move', 'players1', 'players2', 'gameGuardianAmount'],
+    components: {
+        Timer,
+    },
+    data: () => ({
+        MovesToIndex,
+        PlayerToIndex,
+        GameGuardian,
+        sendingPlay: false,
+    }),
+    methods: {
+        choosePlayer(index) {
+            this.$emit('player-chosen', index);
+        },
+        chooseMove(index) {
+            this.$emit('move-chosen', index);
+        },
+        play() {
+            this.sendingPlay = true;
+            this.$emit('play');
+        }
+    }
+}
+</script>
+
+<style>
+.mrgtop {
+    margin-top: 20px;
+}
+</style>
---
+++ client/src/components/HelpMenu.vue
@@ -0,0 +1,57 @@
+
+<template>
+    <v-dialog ref="myDialog"
+        v-model="dialog"
+        width="500"
+        persistent
+    >
+        <v-btn absolute small top right fab
+            class="menu help"
+            slot="activator"
+        >
+            <v-icon>fa-question</v-icon>
+        </v-btn>
+
+        <v-card>
+            <v-card-title
+                class="headline grey darken-4"
+                primary-title
+            >
+                Rock-Paper-Scissors on Raiden Network
+            </v-card-title>
+
+            <v-card-text>
+                <p class="display-1">About</p>
+                <a href="https://github.com/cryptoplayerone/cryptobotwars" target="_blank">Github source code</a>
+                <p>Can be played by an unlimited number of simultaneous users, who choose between the game's two players. There is only 1 game playing at any point in time. You might have to wait for the current game to conclude before being able to play.</p>
+                <p>You choose a player and a move (rock, paper or scissors). The Game Client (web app) sends your encrypted move data to the Game Guardian, and triggers an off-chain payment of <span class="font-weight-black">{{parseFloat(amount / 10**18).toFixed(18)}} WETH</span> on your behalf, from your Raiden node to the Game Guardian address: <a :href="'https://etherscan.io/address/' + gameAddresses.guardian" target="_blank"><span class="font-weight-black">{{gameAddresses.guardian}}</span></a>.</p>
+                <p>The game uses the following token: <a :href="'https://etherscan.io/address/' + gameAddresses.token" target="_blank"><span class="font-weight-black">{{gameAddresses.token}}</span></a>.</p>
+
+                <p>After the game round ends, the web app sends the actual move data. The Game Guardian then choses each player's move based on how the majority voted and choses the winning move & player based on the known rock-paper-scissors rules. It calculates how many tokens it has received for the game, keeps 10% (Robots also want crypto) and then distributes the rest to all players that sent the winning move & player.</p>
+                <p>At the end, <span class="font-weight-black black" > Dark Vader</span> & <span class="font-weight-black blue"> Blue Yoda</span> will act out the duel for you in a live stream (make sure your sound is on). If the live stream is not available, it means they are tired and need to sleepzzz.</p>
+
+                <p class="display-1">How to play</p>
+                <p>To play the game, you need to run a Raiden node on Mainnet. It only works with the <a href="https://github.com/raiden-network/raiden/releases/tag/v0.100.1" target="_blank">Red Eyes release</a>. Check out the <a href="https://raiden-network.readthedocs.io/en/stable/" target="_blank">Raiden Installation Guide</a> for details.</p>
+
+                <p class="red">To be able to receive rewards, you need to make sure you have a path from the Game Guardian <span class="font-weight-black">{{gameAddresses.guardian}}</span> to your node, that has enough deposits in the rewards direction. You may need to ask other nodes to open a channel with you and make a deposit, you can ask for help in <a :href="chatUrl" target="_blank" class="white--text">our chat</a>.</p>
+
+                <p>When running Raiden, you need to set the <span class="font-weight-black">--rpccorsdomain</span> flag to the game's domain.</p>
+                <p class="red">This <span class="font-weight-black">will</span> give access to the game to make payments <span class="font-weight-black">on your behalf</span> when choosing a move. Do not deposit more tokens than you are willing to lose.</p>
+
+                <p>Example:</p>
+                <v-textarea
+                    solo
+                    name="input-7-1"
+                    value='raiden-v0.100.1 --accept-disclaimer --eth-rpc-endpoint https://mainnet.infura.io/v3/[YOUR_TOKEN] --keystore-path ~/Library/Ethereum/keystore --rpc --rpccorsdomain http://127.0.0.1:*/*,https://cryptoplayer.one:*/* --api-address http://127.0.0.1:5001 --address [YOUR_ETHEREUM_ADDRESS]'
+                ></v-textarea>
+
+                <p class="display-1">Settings</p>
+                <p>To play the game, you need to provide your Raiden node <span class="font-weight-black">--api-address</span>.</p>
+                <v-form v-model="valid">
+                    <v-text-field
+                        v-model="ip"
+                        :rules="ipRules"
+                        label="Raiden public API RPC server"
+                        placeholder="127.0.0.1:5001"
+                        :value="userInfo.ip"
+                        :append-icon="ip ? `check` : ``"
