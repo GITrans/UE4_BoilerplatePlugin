1872e4de58ab2527a6ea71574df184c79aac1537
GITrans <109586368+GITrans@users.noreply.github.com>
2021-01-09 07:53:48 +0000

    Refactored the code

--- client/src/components/RockPaperScissors.vue
+++ client/src/components/RockPaperScissors.vue
@@ -2,4 +2,7 @@
     <v-layout row wrap text-xs-center>
         <v-flex xs4>
             <v-btn
-                v-on:click.stop="$emit('move
+                v-on:click.stop="$emit('move-chosen', MovesToIndex.rock)"
+                large fab
+            >
+                <v-icon 
