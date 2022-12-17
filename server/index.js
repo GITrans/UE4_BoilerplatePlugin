cf573a90763e86979116647277ec8c171381f91b
GITrans <109586368+GITrans@users.noreply.github.com>
2022-12-17 12:15:52 +0000

    few comments

--- server/index.js
+++ server/index.js
@@ -1,3 +1,8 @@
 const application = require('./dist');
 
-module.exports = application
+module.exports = application;
+
+if (require.main === module) {
+  // Run the application
+  application.main().catch(err => {
+    console.err
