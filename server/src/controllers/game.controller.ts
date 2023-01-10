837da0f7c9d35fe4a3e1845cdf1bd78c92584ef9
GITrans <109586368+GITrans@users.noreply.github.com>
2023-01-10 05:27:43 +0000

    added context

--- server/src/controllers/game.controller.ts
+++ server/src/controllers/game.controller.ts
@@ -45,3 +45,118 @@ export class GameController {
   getAmount(): GameAmountInterface {
     return {
         amount: TOKEN_AMOUNT_WEI,
+    };
+  }
+
+  @get('/game/create', {
+    responses: {
+      '200': {
+        description: 'Game model instance',
+        content: {'application/json': {schema: {'x-ts-type': Game}}},
+      },
+      '403': {
+        description: 'Previous game has not ended',
+      }
+    },
+  })
+  async create(): Promise<Game> {
+    // Return an existing running game or create a new one
+    let game: Game, newGame: any = {};
+    let now: number, deltaTime: number;
+
+    game = (await this.gameRepository.find({order: ['_id DESC'], limit: 1}))[0];
+
+    if (game) {
+        now = new Date().getTime();
+        deltaTime = game.startTime.getTime() + game.gameTime;
+        if (now < deltaTime) {
+            return game;
+        }
+        if (now < deltaTime + game.resolveTime) {
+            throw new HttpErrors.Forbidden('Previous game has not ended');
+        }
+    }
+
+    newGame.startTime = new Date();
+    newGame.move_amount = TOKEN_AMOUNT_WEI;
+    return await this.gameRepository.create(newGame);
+  }
+
+  @get('/game/count', {
+    responses: {
+      '200': {
+        description: 'Game model count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async count(
+    @param.query.object('where', getWhereSchemaFor(Game)) where?: Where,
+  ): Promise<Count> {
+    return await this.gameRepository.count(where);
+  }
+
+  @get('/game', {
+    responses: {
+      '200': {
+        description: 'Array of Game model instances',
+        content: {
+          'application/json': {
+            schema: {type: 'array', items: {'x-ts-type': Game}},
+          },
+        },
+      },
+    },
+  })
+  async find(
+    @param.query.object('filter', getFilterSchemaFor(Game)) filter?: Filter,
+  ): Promise<Game[]> {
+    return await this.gameRepository.find(filter);
+  }
+
+  @patch('/game', {
+    responses: {
+      '200': {
+        description: 'Game PATCH success count',
+        content: {'application/json': {schema: CountSchema}},
+      },
+    },
+  })
+  async updateAll(
+    @requestBody() game: Game,
+    @param.query.object('where', getWhereSchemaFor(Game)) where?: Where,
+  ): Promise<Count> {
+    return await this.gameRepository.updateAll(game, where);
+  }
+
+  @get('/game/{id}', {
+    responses: {
+      '200': {
+        description: 'Game model instance',
+        content: {'application/json': {schema: {'x-ts-type': Game}}},
+      },
+    },
+  })
+  async findById(@param.path.string('id') id: string): Promise<Game> {
+    // Check if date is > after resolution time
+    // get all game moves sorted -> calculate entire amount received by the game
+    // calculate winning move -> count all the winners -> get amount of guardian ->  amount/winner
+    // connect to Guardian Raiden node -> get all game payments by payment identifiers (identifier > x)
+    // calculate
+    // send Raiden payments to all winners
+    let game: Game;
+    let currentTime, resolveTime, deltaTime;
+    let moves: Move[], validMoves: Move[] = [], winningMoves: Move[] = [];
+    let moveController;
+    let total_amount: number = 0, winner_amount: number, guardian_amount: number;
+    let move_count: any = {
+        '1': {
+            rock: 0,
+            paper: 0,
+            scissors: 0,
+        },
+        '2': {
+            rock: 0,
+            paper: 0,
+            scissors: 0,
+        }
