502e54b7979876b1726978b3a8accc20338aecee
GITrans <109586368+GITrans@users.noreply.github.com>
2023-01-15 06:38:21 +0000

    asdfadsf

--- server/src/controllers/ping.controller.ts
+++ server/src/controllers/ping.controller.ts
@@ -1,2 +1,8 @@
 import {Request, RestBindings, get, ResponseObject} from '@loopback/rest';
-import {inject} fr
+import {inject} from '@loopback/context';
+
+/**
+ * OpenAPI response for ping()
+ */
+const PING_RESPONSE: ResponseObject = {
+  description:
